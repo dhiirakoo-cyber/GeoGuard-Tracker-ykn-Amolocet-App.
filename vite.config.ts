029b5fba38330852c2c137c15dd4d99c98e5e1bc
dhiirakoo-cyber <dhiirakoo@gmail.com>
2026-05-11 17:43:25 +0300

    Update vite.config.tsFix white screen error and configure base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
-
   return {
     base: './',
     plugins: [react(), tailwindcss()],
@@ -22,3 +21,4 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
+
